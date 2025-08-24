fb7f7123a1da440736052b5431edbbf4c9ee3e4b
iscodand <isco.dev@outlook.com>
2025-08-24 10:48:29 -0300

    feature: adicionando migrations

---
+++ src/ApplicationDbContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+using UrlShortener.Models;
+
+namespace UrlShortener
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) 
+            : base(options)
+        { }
+
+        public DbSet<ShortUrl> ShortUrls { get; set; }
+    }
+}
